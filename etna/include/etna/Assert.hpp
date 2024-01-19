fe21c6703ff2d143c2f50a668b4b63300a931e87
JackOfBlades232 <petrkiyashko232@gmail.com>
2024-01-19 12:39:57 +0200

    Added new function for vulkan.hpp result checking & extraction

--- etna/include/etna/Assert.hpp
+++ etna/include/etna/Assert.hpp
@@ -4,21 +4,24 @@
 
 #include <stdexcept>
 #include <spdlog/spdlog.h>
+#include <vulkan/vulkan.hpp>
 
 
 namespace etna
 {
-  struct SourceLocation
-  {
-    std::string_view file;
-    uint32_t line;
-  };
 
-  [[noreturn]] inline void panic(SourceLocation loc, std::string message)
-  {
-    spdlog::critical("Panicked at {}:{}, {}", loc.file, loc.line, message);
-    std::terminate();
-  }
+struct SourceLocation
+{
+  std::string_view file;
+  uint32_t line;
+};
+
+[[noreturn]] inline void panic(SourceLocation loc, std::string message)
+{
+  spdlog::critical("Panicked at {}:{}, {}", loc.file, loc.line, message);
+  std::terminate();
+}
+  
 }
 
 #define ETNA_CURRENT_LOCATION etna::SourceLocation{__FILE__, __LINE__}
@@ -50,7 +53,20 @@ do                                              \
 while (0)
 
 #define ETNA_VK_ASSERTF(expr, fmtStr, ...) ETNA_ASSERTF(expr == vk::Result::eSuccess, fmtStr, __VA_ARGS__)
-#define ETNA_VK_ASSERT(expr) ETNA_ASSERT(expr == vk::Result::eSuccess)
+#define ETNA_VK_ASSERT(expr) ETNA_ASSERT((vk::Result)expr == vk::Result::eSuccess)
+
+
+namespace etna
+{
+
+template <typename T>
+inline T &validate_vk_result(vk::ResultValue<T> res)
+{
+  ETNA_VK_ASSERT(res.result);
+  return res.value;
+}
+
+}
 
 // NOTE: unsanitary macro for customizing vulkan.hpp
 // Do NOT use in app code!
